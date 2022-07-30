d2f59376a7635e80526c73597f0c65f4d2e45841
root <root@nomeno.casa>
2022-07-30 19:03:57 -0300

    Adicionei partials

--- appcatena/views.py
+++ appcatena/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+itens_menu = {
+    1:'Inicial',
+    2:'Cumprimento de MBA',
+    3:'Acompanhamento Operação',
+    4:'Visão por etapa'
+}
+dados ={'titulo_link_menu':itens_menu}
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',dados)
 def mba(request):
-    return render(request,'mba.html')
+    return render(request,'mba.html',dados)
